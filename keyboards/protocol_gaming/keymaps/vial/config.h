c1d5a1892f404e074d7e4bfd9efbe1cd4676865b
dotleon <34742339+dotleon@users.noreply.github.com>
2023-02-16 22:01:06 +0100

    add protocol_gaming keyboard

---
+++ keyboards/protocol_gaming/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD5, 0x13, 0x61, 0x9E, 0x24, 0x73, 0x87, 0x35}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
