16408273cf9227e99ebf50f30ea279ed97506b36
dotleon <34742339+dotleon@users.noreply.github.com>
2023-02-16 22:00:48 +0100

    replace hungarian keycodes in cheapis

--- keyboards/cheapis/keymaps/VIA/keymap.c
+++ keyboards/cheapis/keymaps/VIA/keymap.c
@@ -1,5 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "keymap_hungarian.h"
 
 enum layer_names {
     _1,
@@ -21,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
     [_1] = LAYOUT(
-    HU_Q   ,HU_W   ,HU_E   ,HU_R   ,HU_T           ,HU_Z   ,HU_U   ,HU_I   ,HU_O   ,HU_P   ,
-    HU_A   ,HU_S   ,HU_D   ,HU_F   ,HU_G           ,HU_H   ,HU_J   ,HU_K   ,HU_L   ,KC_5   ,
-    HU_Y   ,HU_X   ,HU_C   ,HU_V   ,HU_B           ,HU_N   ,HU_M   ,HU_COMM,HU_DOT ,KC_6   ,
+    KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T           ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,
+    KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G           ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_5   ,
+    KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B           ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_6   ,
                             KC_1   ,KC_2           ,KC_3   ,KC_4
     ),
     [_2] = LAYOUT(
