32d3d8250a7e1e02b16572ce685141b5fcae0da6
dotleon <34742339+dotleon@users.noreply.github.com>
2023-08-06 21:18:22 +0200

    50keylove2 fw working

---
+++ keyboards/50keylove2/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5D, 0xF2, 0x11, 0xFF, 0x86, 0x3C, 0xC2, 0x7A}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
---
+++ keyboards/50keylove2/keymaps/vial/rules.mk
@@ -0,0 +1,4 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+MOUSEKEY_ENABLE = no
+LTO_ENABLE = yes
